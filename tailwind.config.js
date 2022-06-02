5f4b831c56456ade25636204e0c57bbb500d4ec0
Rauno Tegelmann <rauno@optimistdigital.com>
2022-06-02 17:17:20 +0300

    Move to parcel pipeline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,7 @@
 module.exports = {
-  purge: [
-    './**/*.html',
-    './**/*.css'
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    fontFamily: {
-      'body': 'Bebas Neue, cursive'
-    },
-    extend: {
-      colors: {
-        yellow: '#efe8ba',
-      },
-      fontSize: {
-        '7xl': '5rem',
-        '8xl': '7rem'
-      },
-      screens: {
-        'xl-down': {'max': '1279px'},
-        'lg-down': {'max': '1023px'},
-        'md-down': {'max': '767px'},
-        'sm-down': {'max': '639px'}
-      },
-      spacing: {
-        '72': '18rem',
-        '84': '21rem',
-        '96': '24rem'
-      }
-    },
+    extend: {},
   },
-  variants: {},
-  plugins: []
-}
+  plugins: [],
+};
