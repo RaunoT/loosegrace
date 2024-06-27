67ab4fd560ea317eb494d55bcbea3b0dbf086b31
Rauno Tegelmann <RaunoT@users.noreply.github.com>
2024-06-27 12:34:23 +0300

    feat: tattoo single release (#14)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     container: {
       center: true,
     },
     fontFamily: {
-      body: "Bebas Neue, cursive",
+      body: 'Bebas Neue, cursive',
     },
     extend: {
       colors: {
-        blonde: "#eee8b4",
-        purple: "#4f2f50",
+        blonde: '#eee8b4',
+        purple: '#4f2f50',
+        tattoo: '#856868',
       },
     },
   },
   plugins: [],
-};
+}
