31c80aa59536bed6caa78260517daf80316a3311
RaunoT <raunoot@gmail.com>
2026-01-19 11:51:01 +0200

    feat: add pre-save button for faded

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,6 @@ module.exports = {
     },
     extend: {
       colors: {
-        blonde: '#eee8b4',
-        purple: '#4f2f50',
-        tattoo: '#856868',
         faded: '#ADDFFF',
       },
     },
